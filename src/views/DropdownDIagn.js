76f3809a4dbd55aca16a8d8a0db2a0406e3a55d7
Stephane_1 <kikonistephane@gmail.com>
2021-09-02 09:56:38 +0100

    fin

--- src/views/DropdownDIagn.js
+++ src/views/DropdownDIagn.js
@@ -12,12 +12,11 @@ function DropdownDIagn(props) {
 
     let data = props.data; // Récupération de data split pour les afficher dans la liste déroulante
 
-    let finSearch = [5];
-    finSearch[0] = props.symptomInput;
+   // let finSearch = [props.symptomInput];
+    const [finSearch, setFinSearch] = useState([props.symptomInput]);
+    //finSearch[0] = props.symptomInput;
 
     let history = useHistory();
-
-    //console.log(history)
 
     let dataComplet = props.dataComplet; // DataComplet trouvé sur base de l'input saisi par le user
 
@@ -25,76 +24,67 @@ function DropdownDIagn(props) {
 
     const handleSelectSymptome = (e) => {
         setSymptomeSelected(e.target.value); // Récupération de la valeur selectionée
-
+        //console.log(symptomeSelected)
         if (symptomeSelected !== undefined) {
             const filterSic = dataComplet.filter(sympt => sympt.symptomes.includes(symptomeSelected));
             setDataFind(filterSic);
             console.log("Symptome selectionne", e.target.value)
 
             if (symptomeSelected !== e.target.value) {
-                /*if (finSearch.includes(e.target.value) === false && finSearch.length === 4) {
-                    finSearch[5] = e.target.value;
+                    //finSearch.push(e.target.value);
+                    setFinSearch([...finSearch,e.target.value])
                     console.log("Tableau d'éléments pushés", finSearch)
-                }
-                if (finSearch.includes(e.target.value) === false && finSearch.length === 2) {
-                    finSearch[3] = e.target.value;
-                    console.log("Tableau d'éléments pushés", finSearch)
-                }*/
-            }
 
-
-            if (finSearch.length < 3) {
-                setHide(true)
-
-                for (let i = 0; i < dataComplet.length; i++) {
-                    var verif = dataComplet[i].symptomes.includes(finSearch)
-                    //console.log(verif)
                 }
 
-                if (newTab1.includes(finSearch)) {
-                    alert("FIN COP");
+
+                if (finSearch.length < 3) {
+                    setHide(true)
+
+                    for (let i = 0; i < dataComplet.length; i++) {
+                        var verif = dataComplet[i].symptomes.includes(finSearch)
+                        //console.log(verif)
+                    }
+
+
                 }
-                setTimeout(() => {
-                    history.push('/Response')
-                }, 4000)
             }
         }
-    }
 
 
-    const newTabS = (data) => {
-        for (let i = 0; i < 4; i++) {
-            if (data) {
-                if (data[2]) {
-                    newTab1 = data[0].concat(data[1], data[2])
-                    return newTab1;
-                } else if (data[3]) {
-                    newTab1 = data[0].concat(data[1], data[2], data[3]);
-                    return newTab1;
+        const newTabS = (data) => {
+            for (let i = 0; i < 4; i++) {
+                if (data) {
+                    if (data[2]) {
+                        newTab1 = data[0].concat(data[1], data[2])
+                        return newTab1;
+                    } else if (data[3]) {
+                        newTab1 = data[0].concat(data[1], data[2], data[3]);
+                        return newTab1;
+                    }
+                    else {
+                        newTab1 = data[0].concat(data[1]);
+                        return newTab1;
+                    }
+                } else {
+                    alert("Non la valeur est indéfinie");
+                    return false;
                 }
-                else {
-                    newTab1 = data[0].concat(data[1]);
-                    return newTab1;
-                }
-            } else {
-                alert("Non la valeur est indéfinie");
-                return false;
             }
         }
+
+        console.log("Nouveau tableau fusionné", newTabS(data))
+        return (
+            <div>
+                <select value={symptomeSelected} onChange={handleSelectSymptome} className="form-control">
+                    {
+                        newTab1.map((el, index) => {
+                            return <option key={index} value={el}>{el}</option>
+                        })
+                    }
+                </select>
+            </div>
+        )
     }
 
-    console.log("Nouveau tableau fusionné", newTabS(data))
-    return (
-        <div>
-            <select value={symptomeSelected} onChange={handleSelectSymptome} className="form-control">
-                {
-                    newTab1.map((el, index) => {
-                        return <option key={index} value={el}>{el}</option>
-                    })
-                }
-            </select>
-        </div>
-    )
-}
-
-export default DropdownDIagn
+    export default DropdownDIagn
