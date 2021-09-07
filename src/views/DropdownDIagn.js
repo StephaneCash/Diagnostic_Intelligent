1f20db8d6ac8e055c20cbc84cb0f560a6f727174
Stephane_1 <kikonistephane@gmail.com>
2021-09-07 15:32:09 +0100

    x

--- src/views/DropdownDIagn.js
+++ src/views/DropdownDIagn.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react'
-import maladieFind from './maladieFind';
 import { useHistory } from "react-router-dom";
 
 function DropdownDIagn(props) {
